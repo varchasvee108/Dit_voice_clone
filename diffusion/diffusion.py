6ef0a4a003175fda5258eee2d4da07087e73b7a0
varchasvi <varchasvee108@gmail.com>
2026-04-10 10:43:58 +0530

    diffusion code done

--- diffusion/diffusion.py
+++ diffusion/diffusion.py
@@ -1,12 +1,11 @@
 import torch
-import torch.nn as nn
 from core.config import Config
 
 
-class DiffusionModel(nn.Module):
+class DiffusionModel:
     def __init__(self, config: Config, device):
-        super().__init__()
         self.config = config
+        self.device = device
         self.betas = torch.linspace(
             config.diffusion.beta_start,
             config.diffusion.beta_end,
@@ -15,3 +14,42 @@ class DiffusionModel(nn.Module):
         )
         self.alphas = 1.0 - self.betas
         self.alpha_cumprod = torch.cumprod(self.alphas, dim=0)
+
+    def sample_timesteps(self, batch_size: int):
+        return torch.randint(
+            0,
+            self.config.diffusion.timesteps,
+            size=(self.config.data.batch_size,),
+            device=self.device,
+        )
+
+    def q_sample(self, x0, t, noise):
+        alpha_hat = self.alpha_cumprod[t]
+        sqrt_alpha_hat = torch.sqrt(alpha_hat).view(-1, 1, 1)
+        sqrt_one_minus = torch.sqrt(1 - alpha_hat).view(-1, 1, 1)
+
+        return sqrt_alpha_hat * x0 + sqrt_one_minus * noise
+
+    def p_sample(self, model, x, t):
+
+        beta = self.betas[t].view(-1, 1, 1)
+        alpha = self.alphas[t].view(-1, 1, 1)
+        alpha_hat = self.alpha_cumprod[t].view(-1, 1, 1)
+        noise_pred = model(x, t)
+
+        mean = (1 / torch.sqrt(alpha)) * x - (
+            (1 - alpha) / torch.sqrt(1 - alpha_hat)
+        ) * noise_pred
+
+        noise = torch.randn_like(x)
+        mask = (t > 0).float().view(-1, 1, 1)
+        return mean + mask * torch.sqrt(beta) * noise
+
+    @torch.no_grad()
+    def sample(self, model, shape):
+        x = torch.randn(shape, device=self.device)
+
+        for t in reversed(range(self.config.diffusion.timesteps)):
+            t_batch = torch.full((shape[0],), t, device=self.device, dtype=torch.long)
+            x = self.p_sample(model, x, t_batch)
+        return x
